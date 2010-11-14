cd4a08404f45e4601643393d9dd944241ea2901e
Zach Tibbitts <zachtib@gmail.com>
2010-11-13 19:19:00 -0500

    Add defaults to LookitConfig

--- src/lookitconfig.py
+++ src/lookitconfig.py
@@ -1,7 +1,39 @@
 from ConfigParser import RawConfigParser
 import keyring
+import os
+import subprocess
+
+try:
+        PICTURE_DIR = subprocess.Popen(['xdg-user-dir', 'PICTURES'], \
+				stdout=subprocess.PIPE).communicate()[0] \
+				.strip('\n')
+except OSError:
+        PICTURE_DIR = os.path.expanduser('~')
 
 class LookitConfig(RawConfigParser):
+        def __init__(self):
+                RawConfigParser.__init__(self)
+                self.load_defaults()
+
+        def load_defaults(self):
+                self.add_section('General')
+                self.set('General', 'shortenurl', False)
+                self.set('General', 'trash', False)
+                self.set('General', 'savedir', PICTURE_DIR)
+                
+                self.add_section('Hotkeys')
+                self.set('Hotkeys', 'caparea', '<Ctrl><Super>4')
+                self.set('Hotkeys', 'capscreen', '<Ctrl><Super>3')
+                
+                self.add_section('Upload')
+                self.set('Upload', 'type', 'None')
+                self.set('Upload', 'server', '')
+                self.set('Upload', 'port', 0)
+                self.set('Upload', 'username', '')
+                self.set('Upload', 'directory', '')
+                self.set('Upload', 'url', 'http://')
+
+
 	def get(self, section, option):
 		if option == 'password':
 			return keyring.get_password('lookit', 'lookit')
@@ -21,3 +53,8 @@ class LookitConfig(RawConfigParser):
                         self.set(new_name, name, value)
                 self.remove_section(old_name)
                 return True
+
+if __name__ == '__main__':
+        lc = LookitConfig()
+        lc.load_defaults()
+
