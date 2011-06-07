2cc72f4d36ef5eee20402b91f5e75b88114091e1
Zach Tibbitts <zachtib@gmail.com>
2011-06-07 00:31:25 -0400

    Cancel selection with Escape

--- src/selector.py
+++ src/selector.py
@@ -18,14 +18,17 @@ class Selector:
 
         self.overlay.add_events(gtk.gdk.POINTER_MOTION_MASK |
                                 gtk.gdk.BUTTON_PRESS_MASK |
-                                gtk.gdk.BUTTON_RELEASE_MASK)
+                                gtk.gdk.BUTTON_RELEASE_MASK |
+                                gtk.gdk.KEY_PRESS_MASK)
 
         self.overlay.connect('expose-event',            self.expose)
         self.overlay.connect('screen-changed',          self.screen_changed)
         self.overlay.connect('realize',                 self.realize)
+        self.overlay.connect('show',                    self.on_show)
         self.overlay.connect('button-press-event',      self.button_pressed)
         self.overlay.connect('button-release-event',    self.button_released)
         self.overlay.connect('motion-notify-event',     self.motion_notify)
+        self.overlay.connect('key-press-event',         self.key_pressed)
 
     def expose(self, widget, event=None):
         cr = widget.window.cairo_create()
@@ -55,6 +58,10 @@ class Selector:
 
     def realize(self, widget):
         widget.window.set_cursor(gtk.gdk.Cursor(gtk.gdk.CROSS))
+        gtk.gdk.keyboard_grab(widget.window)
+
+    def on_show(self, widget):
+        gtk.gdk.keyboard_grab(widget.window)
 
     def button_pressed(self, widget, event):
         self.mouse_down = True
@@ -70,6 +77,12 @@ class Selector:
         self.dy = event.y - self.y
 
         self.expose(self.overlay)
+
+    def key_pressed(self, widget, event):
+        if event.keyval == gtk.gdk.keyval_from_name('Escape'):
+            gtk.main_quit()
+        else:
+            return False
 
     def get_selection(self):
         self.screen_changed(self.overlay)
