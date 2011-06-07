343b2564c10ba7dba774cda1fa8f7bfe6e612f3d
Zach Tibbitts <zachtib@gmail.com>
2011-06-06 23:47:08 -0400

    break imgur code into it's own library

---
+++ src/imgur.py
@@ -0,0 +1,43 @@
+import pycurl
+import xml.parsers.expat
+
+class ImgurUploader:
+    def __init__(self):
+        self.response = ''
+        self.current_key = None
+        self.mapping = {}
+
+    def xml_ele_start(self, name, attrs):
+        self.current_key = str(name)
+
+    def xml_ele_end(self, name):
+        self.current_key = None
+
+    def xml_ele_data(self, data):
+        if self.current_key is not None:
+            self.mapping[self.current_key] = str(data)
+
+    def curl_response(self, buf):
+        self.response = self.response + buf
+
+    def upload(self, image):
+        c = pycurl.Curl()
+        # Note: This key is specific for Lookit. If you want to use
+        # the Imgur API in your application, please apply for an
+        # API key at: http://imgur.com/register/api/
+        values = [  ('key', 'e5acb0d99a09b654a2c7e833c7f2dbe1'),
+                ('image', (c.FORM_FILE, image))]
+        c.setopt(c.URL, 'http://imgur.com/api/upload.xml')
+        c.setopt(c.HTTPPOST, values)
+        c.setopt(c.WRITEFUNCTION, self.curl_response)
+
+        c.perform()
+        c.close()
+
+        p = xml.parsers.expat.ParserCreate()
+
+        p.StartElementHandler = self.xml_ele_start
+        p.EndElementHandler = self.xml_ele_end
+        p.CharacterDataHandler = self.xml_ele_data
+
+        p.Parse(self.response)
