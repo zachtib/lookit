9ca1dc737a4da1c67235c8d0d0ae634c43b5bd0d
Zach Tibbitts <zachtib@gmail.com>
2011-06-09 18:59:46 -0400

    Added selector non-composited fallback (but it's a bit laggy at the moment)

--- src/selector.py
+++ src/selector.py
@@ -2,6 +2,8 @@ import cairo
 import gtk
 import gtk.gdk
 
+import screencapper
+
 class Selector:
     def __init__(self):
         self.x = 0
@@ -9,8 +11,11 @@ class Selector:
         self.dx = 0
         self.dy = 0
 
+        self.is_composited = False
         self.supports_alpha = False
         self.mouse_down = False
+
+        self.pixbuf = None
 
         self.overlay = gtk.Window(gtk.WINDOW_POPUP)
 
@@ -32,8 +37,8 @@ class Selector:
         self.overlay.connect('key-press-event',         self.key_pressed)
 
     def expose(self, widget, event=None):
-        if self.supports_alpha:
-            cr = widget.window.cairo_create()
+        cr = widget.window.cairo_create()
+        if self.is_composited and self.supports_alpha:
             cr.set_source_rgba(0.125, 0.125, 0.125, 0.75)
 
             cr.set_operator(cairo.OPERATOR_SOURCE)
@@ -45,11 +50,22 @@ class Selector:
                 cr.fill()
                 cr.stroke()
         else:
-            widget.hide_all()
+            cr.set_source_pixbuf(self.pixbuf, 0, 0)
+            cr.paint()
+
+            cr.set_operator(cairo.OPERATOR_SOURCE)
+
+            if self.mouse_down:
+                cr.set_source_rgba(1.0, 0, 0, 0)
+                cr.rectangle(self.x, self.y, self.dx, self.dy)
+                cr.stroke()
         return False
 
     def screen_changed(self, widget, old_screen=None):
         screen = widget.get_screen()
+        self.is_composited = screen.is_composited()
+        if not self.is_composited:
+            self.pixbuf = screencapper.ScreenCapper().capture_screen()
         widget.move(0, 0)
         widget.resize(screen.get_width(), screen.get_height())
 
@@ -59,6 +75,7 @@ class Selector:
             self.supports_alpha = False
         else:
             self.supports_alpha = True
+
         widget.set_colormap(colormap)
 
         return True
@@ -69,6 +86,8 @@ class Selector:
 
     def on_show(self, widget):
         gtk.gdk.keyboard_grab(widget.window)
+        if not self.is_composited:
+            pass
 
     def button_pressed(self, widget, event):
         self.mouse_down = True
@@ -107,8 +126,7 @@ class Selector:
 
     def get_selection(self):
         self.screen_changed(self.overlay)
-        if self.supports_alpha:
-            self.overlay.show_all()
+        self.overlay.show_all()
         gtk.main()
         return self.selection_rectangle()
 
