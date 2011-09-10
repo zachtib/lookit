23d5691f4c7f45c09568302642434c7b45c2f297
Zach Tibbitts <zachtib@gmail.com>
2011-09-10 14:23:59 -0400

    More additions to the dbus service.

--- lookit/lookitservice.py
+++ lookit/lookitservice.py
@@ -3,6 +3,9 @@
 import dbus
 import dbus.service
 import gobject
+import os
+import thread
+import time
 
 from dbus.mainloop.glib import DBusGMainLoop
 DBusGMainLoop(set_as_default=True)
@@ -41,7 +44,35 @@ class LookitService(dbus.service.Object):
     def upload_file(self, filename):
         print('Uploading {0}...'.format(filename))
 
-if __name__ == '__main__':
-    service = LookitService()
-    service.start()
+    @dbus.service.method(SERVICE_NAME)
+    def show_preferences_dialog(self):
+        pass
 
+    @staticmethod
+    def get_service_or_start():
+        print('Starting...')
+        bus = dbus.SessionBus()
+        if SERVICE_NAME not in bus.list_names():
+            print('Forking the service...')
+            pid = os.fork()
+            print pid
+            if pid == 0:
+                LookitService().start()
+            else:
+                while SERVICE_NAME not in bus.list_names():
+                    try:
+                        if os.waitpid(pid, os.WNOHANG) != (0, 0):
+                            return None
+                    except OSError as e:
+                        if e.errno == errno.ECHILD:
+                            return None
+                    time.sleep(0.1)
+        print('Now connecting...')
+        service = bus.get_object(SERVICE_NAME, SERVICE_PATH)
+        return service
+
+if __name__ == '__main__':
+    print('Attempting to connect to dbus service...')
+    service = LookitService.get_service_or_start()
+    service.get_dbus_method('upload_file', SERVICE_NAME)('Banana Phone')
+    service.get_dbus_method('stop', SERVICE_NAME)()
